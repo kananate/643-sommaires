8da869a98adb7f418a5ab0afd7f7972f45ff68d8
kananate <papipoune@yahoo.fr>
2025-11-09 00:50:19 +0100

    update

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@
 
 const mainEle = document.querySelectorAll("h2");
 const conEle = document.querySelectorAll(".chapitre");
-const foot = document.querySelector(".display-f");
+
 
 for (let x = 0; x < mainEle.length; x++) {
     mainEle[x].addEventListener("click", function () {
         console.log(mainEle[x].firstElementSibling);
         clearActive();        
         mainEle[x].nextElementSibling.classList.toggle("chapitre");        
-        foot.classList.toggle("display-f");
+        
         
         })
 }
